5c6dcbbd3f9fae9fe4b2fa586b304700ae1d79a2
yashveerSisodia <sisodia.yash98@gmail.com>
2023-09-13 17:59:26 +0530

    first commit

---
+++ src/main/java/com/yash/practiceapp/PracticeAppApplication.java
@@ -0,0 +1,13 @@
+package com.yash.practiceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/practiceapp/PracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.practiceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
